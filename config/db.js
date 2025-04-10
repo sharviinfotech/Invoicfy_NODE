a4971d2c719135ae7de1ee5d1270abba0e7f725c
Sunil Kumar <sunilkumar@sharviinfotech.com>
2025-04-10 15:35:05 +0530

    changes in db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://paramesh:do7zSGvCunwKJORR@cluster0.e4oz0ms.mongodb.net/Sharvi_Invoice";
-// const uri = "mongodb://SharviDb:Sharvi%401234@192.168.1.4:27017/Invoice?authSource=admin&ssl=false";
+// const uri = "mongodb+srv://paramesh:do7zSGvCunwKJORR@cluster0.e4oz0ms.mongodb.net/Sharvi_Invoice";
+const uri = "mongodb://SharviDb:Sharvi%401234@192.168.1.4:27017/Invoice?authSource=admin&ssl=false";
 
 const connectDB = async () => {
     try {
